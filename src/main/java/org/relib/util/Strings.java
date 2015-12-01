c416ce8349cab2650f9e974e84c59d4972ee2f4a
Troy <TroyHisted@users.noreply.github.com>
2015-12-01 12:57:00 -0500

    Adding a Strings utility class.

---
+++ src/main/java/org/relib/util/Strings.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright 2015 Troy Histed
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.relib.util;
+
+/**
+ * Utility method for manipulating strings.
+ *
+ * @author Troy Histed
+ */
+public class Strings {
+
+	/**
+	 * An empty string ("");
+	 */
+	public static final String EMPTY = "";
+
+	/**
+	 * Null safe trim.
+	 *
+	 * @param string the string to trim
+	 * @return the trimmed string or null if the passed in string was null
+	 */
+	public static String trim(String string) {
+		if (string != null) {
+			return string.trim();
+		}
+		return null;
+	}
+
+	/**
+	 * Null safe trim.
+	 *
+	 * <p>
+	 * Specify the characters to trim. All characters in the string that match the specified characters
+	 * will be removed from the start and end of the string.
+	 *
+	 * @param string the string to trim
+	 * @param characters the characters to remove
+	 * @return trimmed string or null if the original string was null
+	 */
+	public static String trim(String string, char... characters) {
+
+		if (string == null) {
+			return null;
+		} else if (string.isEmpty() || characters == null || characters.length == 0) {
+			return string;
+		}
+
+		final char[] stringChars = string.toCharArray();
+		int startIndex = 0;
+		nextCharacter:
+		while (startIndex < stringChars.length) {
+			for (final char character : characters) {
+				if (stringChars[startIndex] == character) {
+					startIndex++;
+					continue nextCharacter;
+				}
+			}
+			break;
+		}
+
+		int endIndex = stringChars.length - 1;
+		previousCharacter:
+		while (endIndex > startIndex) {
+			for (final char character : characters) {
+				if (stringChars[endIndex] == character) {
+					endIndex--;
+					continue previousCharacter;
+				}
+			}
+			break;
+		}
+
+		return string.substring(startIndex, endIndex + 1);
+	}
+
+	/**
+	 * Determines if a string contains any text.
+	 *
+	 * <p>
+	 * Returns true if, after trimming whitespace, the string is empty or null.
+	 *
+	 * <ul>
+	 * <li>Null returns true
+	 * <li>EMPTY returns true
+	 * <li>Whitespace returns true
+	 * </ul>
+	 *
+	 * @param string the string to test
+	 * @return <code>true</code> if the string contains any text
+	 */
+	public static boolean isBlank(String string) {
+		return string == null || EMPTY.equals(Strings.trim(string));
+	}
+
+	/**
+	 * Tests a string to determine if it contains only numeric characters.
+	 *
+	 * <p>
+	 * An empty string is considered numeric. Decimal points are not numeric characters. Null returns false.
+	 *
+	 * @param string the string to test
+	 * @return <code>true</code> if the specified string is numeric
+	 */
+	public static boolean isNumeric(String string) {
+		if (string == null) {
+			return false;
+		}
+		final char[] stringChars = string.toCharArray();
+		for (final char character : stringChars) {
+			if (character < '0' || character > '9') {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Null safe substring.
+	 *
+	 * <p>
+	 * @see String#substring(int, int)
+	 *
+	 * @param string the string to substring
+	 * @param start the starting index
+	 * @param end the ending index
+	 * @return either null or the substring
+	 */
+	public static String subString(String string, int start, int end) {
+		return string == null ? null : string.substring(start, end);
+	}
+}
---
+++ src/test/java/org/relib/util/StringsTest.java
@@ -0,0 +1,220 @@
+/**
+ * Copyright 2015 Troy Histed
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.relib.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Tests the {@link Strings} class.
+ *
+ * @author Troy Histed
+ */
+public class StringsTest {
+
+	/**
+	 * Verify a trim works with a null string.
+	 */
+	@Test
+	public void testTrimNull() {
+		Assert.assertNull(Strings.trim(null));
+	}
+
+	/**
+	 * Verify a trim works with an empty string.
+	 */
+	@Test
+	public void testTrimEmpty() {
+		Assert.assertEquals("", Strings.trim(""));
+	}
+
+	/**
+	 * Verify a trim works with a nothing to trim string.
+	 */
+	@Test
+	public void testTrimNothing() {
+		Assert.assertEquals("abc", Strings.trim("abc"));
+	}
+
+	/**
+	 * Verify a trim works with a leading trims.
+	 */
+	@Test
+	public void testTrimLeading() {
+		Assert.assertEquals("abc", Strings.trim("  abc"));
+	}
+
+	/**
+	 * Verify a trim works with a trailing trims.
+	 */
+	@Test
+	public void testTrimTrailing() {
+		Assert.assertEquals("abc", Strings.trim("abc  "));
+	}
+
+	/**
+	 * Verify a trim works on both ends.
+	 */
+	@Test
+	public void testTrimBothSides() {
+		Assert.assertEquals("abc", Strings.trim(" abc "));
+	}
+
+	/**
+	 * Verify a trim works with a null string.
+	 */
+	@Test
+	public void testTrimNullSlash() {
+		Assert.assertNull(Strings.trim(null, '/'));
+	}
+
+	/**
+	 * Verify a trim works with an empty string.
+	 */
+	@Test
+	public void testTrimEmptySlash() {
+		Assert.assertEquals("", Strings.trim("", '/'));
+	}
+
+	/**
+	 * Verify a trim works with a nothing to trim string.
+	 */
+	@Test
+	public void testTrimNothingSlash() {
+		Assert.assertEquals("abc", Strings.trim("abc", '/'));
+	}
+
+	/**
+	 * Verify a trim works with a leading trims.
+	 */
+	@Test
+	public void testTrimLeadingSlash() {
+		Assert.assertEquals("abc", Strings.trim("//abc", '/'));
+	}
+
+	/**
+	 * Verify a trim works with a trailing trims.
+	 */
+	@Test
+	public void testTrimTrailingSlash() {
+		Assert.assertEquals("abc", Strings.trim("abc//", '/'));
+	}
+
+	/**
+	 * Verify a trim works with only the specified character being trimmed.
+	 */
+	@Test
+	public void testTrimOnlySlash() {
+		Assert.assertEquals("", Strings.trim("////", '/'));
+	}
+
+	/**
+	 * Verify a trim works with for multiple characters.
+	 */
+	@Test
+	public void testTrimMultipleCharacters() {
+		Assert.assertEquals("abc", Strings.trim("/\\abc/\\", '/', '\\'));
+	}
+
+	/**
+	 * Verify isBlank works with null.
+	 */
+	@Test
+	public void testIsBlankNull() {
+		Assert.assertTrue(Strings.isBlank(null));
+	}
+
+	/**
+	 * Verify isBlank works with an empty string.
+	 */
+	@Test
+	public void testIsBlankEmpty() {
+		Assert.assertTrue(Strings.isBlank(""));
+	}
+
+	/**
+	 * Verify isBlank works with a whitespace string.
+	 */
+	@Test
+	public void testIsBlankWhitespace() {
+		Assert.assertTrue(Strings.isBlank("  "));
+	}
+
+	/**
+	 * Verify isBlank works with a character in the string.
+	 */
+	@Test
+	public void testIsBlankCharacter() {
+		Assert.assertFalse(Strings.isBlank(" - "));
+	}
+
+	/**
+	 * Verify isNumeric is false with null.
+	 */
+	@Test
+	public void testIsNumericNull() {
+		Assert.assertFalse(Strings.isNumeric(null));
+	}
+
+	/**
+	 * Verify isNumeric is true with an empty string.
+	 */
+	@Test
+	public void testIsNumericEmpty() {
+		Assert.assertTrue(Strings.isNumeric(""));
+	}
+
+	/**
+	 * Verify isNumeric is false with whitespace.
+	 */
+	@Test
+	public void testIsNumericWhitespace() {
+		Assert.assertFalse(Strings.isNumeric(" "));
+	}
+
+	/**
+	 * Verify isNumeric works with a numeric string.
+	 */
+	@Test
+	public void testIsNumericWithNumeric() {
+		Assert.assertTrue(Strings.isNumeric("0987654321"));
+	}
+
+	/**
+	 * Verify isNumeric fails with an alphanumeric string.
+	 */
+	@Test
+	public void testIsNumericAlphanumeric() {
+		Assert.assertFalse(Strings.isNumeric("09876a54321"));
+	}
+
+	/**
+	 * Verify substring handles null.
+	 */
+	@Test
+	public void testSubstringNull() {
+		Assert.assertNull(Strings.subString(null, 0, 9));
+	}
+
+	/**
+	 * Verify substring handles a string.
+	 */
+	@Test
+	public void testSubstringString() {
+		Assert.assertEquals("b", Strings.subString("abc", 1, 2));
+	}
+
+}
